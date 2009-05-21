070998c993071bfc842633bcaa13915b5db89974
Matt Porter <mporter@embeddedalley.com>
2009-05-21 10:44:12 -0400

    Enable vold support on BB. Note: Be sure to use update kernel defconfig!

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -25,6 +25,11 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := usbkbd.kcm
 include $(BUILD_KEY_CHAR_MAP)
 
+file := $(TARGET_ROOT_OUT)/system/etc/vold.conf
+$(file): $(LOCAL_PATH)/vold.conf | $(ACP)
+	$(transform-prebuilt-to-target)
+ALL_PREBUILT += $(file)
+
 file := $(TARGET_ROOT_OUT)/system/media/audio/ui/boot.wav
 $(file): $(LOCAL_PATH)/boot.wav | $(ACP)
 	$(transform-prebuilt-to-target)
