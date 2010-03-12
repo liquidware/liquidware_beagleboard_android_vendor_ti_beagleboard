46caa0aada40a41b18621c974045b27f40e717cc
Yegor Yelizarov <yegor_yelizarov@mentor.com>
2010-03-12 17:17:50 +0300

    Fix long name issue
    Change-Id: Ia5cc8bdcab6e00938705dddbc05816ebb84f1b5f
    Signed-off-by: Yegor Yelizarov <yegor_yelizarov@mentor.com>

--- beagleboard.mk
+++ beagleboard.mk
@@ -5,6 +5,6 @@ $(call inherit-product, build/target/product/rowboat.mk)
 
 # Overrides
 PRODUCT_BRAND := TI
-PRODUCT_NAME := beagle
-PRODUCT_DEVICE := beagle
+PRODUCT_NAME := beagleboard
+PRODUCT_DEVICE := beagleboard
 PRODUCT_PACKAGE_OVERLAYS := vendor/rowboat/generic
